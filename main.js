bbec5cee4b7a1f6df472396989bcd42a83a5ff1c
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-22 17:28:33 -0400

    commit tree a8bed6dccad2349758c4388e218387832fabb8e9

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', event => {
+  let hearts = document.getElementsByClassName("like-glyph")
+  for (heart of hearts) {
+  heart.addEventListener('click', likeHearts)
+  }
+})
+
+function likeHearts(event) {
+  mimicServerCall()
+    .then(() => {
+      let e = event.target;
+      if (e.textContent == EMPTY_HEART) {
+        e.classList.add('activated-heart');
+        e.textContent = FULL_HEART;
+      } else if (e.textContent == FULL_HEART) {
+        e.classList.remove('activated-heart');
+        e.textContent = EMPTY_HEART;
+      }
+    })
+    .catch(error => {
+      document.querySelector('#modal').classList.remove('hidden');
+      document.querySelector('#modal').textContent = `${error}`;
+      setTimeout(function(){ document.querySelector('#modal').classList.add('hidden')}, 3000);
+  })
+}
+
 
 
 
